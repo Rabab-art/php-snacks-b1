80f7d9799c540da090af22fccc8dfb5ba101fac8
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-23 17:34:18 +0200

    Lista di numeri random

---
+++ slack2.php
@@ -0,0 +1,13 @@
+<!-- SNACK 2
+Creare un array contenente qualche alunno di un’ipotetica classe.
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici (senza, materia, solo numeri). Creare una funzione che calcoli la media voto di un alunno.
+Stampare Nome, Cognome e la media dei voti di ogni alunno.
+
+
+BONUS SNACK
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione)
+-che name sia più lungo di 3 caratteri,
+-che mail contenga un punto e una chiocciola
+- che age sia un numero.
+-Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+SUPER BONUS: Personalizzare l'avviso di accesso negato con la motivazione. es: ("Accesso negato: mail non valida") -->
