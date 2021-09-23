a1b81d79710c210cd5a7fe1413899f76c21d3cda
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-23 18:00:21 +0200

    Creazione arrei di studenti con nome, cognome e media

--- slack2.php
+++ slack2.php
@@ -11,3 +11,43 @@ Passare come parametri GET name, mail e age e verificare (cercando i metodi che 
 - che age sia un numero.
 -Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 SUPER BONUS: Personalizzare l'avviso di accesso negato con la motivazione. es: ("Accesso negato: mail non valida") -->
+<?php $students = [
+    [
+      'nome' => 'Lola',
+      'cognome' => 'Pina',
+      'media'  => 6.5,
+    ],
+    [
+      'nome' => 'Amira',
+      'cognome' => 'Elsamir',
+      'media' => 8,
+    ],
+    [
+        'nome' => 'Youssef',
+        'cognome' => 'Buras',
+        'media' => 7,
+      ],
+      [
+        'nome' => 'Fatima',
+        'cognome' => 'Ricci',
+        'media' => 9,
+      ],
+
+];
+
+$somma = array_sum($students);
+  ?>
+
+  <ul>
+    <?php for ($i = 0; $i < count($students); $i++) { ?>
+      <li>
+        <h2><?= $students[$i]['nome'] . ' ' . $students[$i]['cognome'] ?></h2>
+        <small><?= $students[$i]['age'] ?></small>
+      </li>
+
+      <?php $media=$somma/ count ($students); ?>
+
+
+    <?php echo 'La media è:' . $media} ?>
+  </ul>
+
